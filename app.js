87efc8ca72dbfa0101307282c5b920634dfb11d3
Juliya Koltsova <juliyaasking009@gmail.com>
2023-08-23 19:04:56 +0200

    feat: сделать логирование

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 
 const router = require('./routers/index');
+
 const serverError = require('./middleware/serverError');
 const limiter = require('./middleware/limiter');
+const {
+  requestLogger,
+  errorLogger,
+} = require('./middleware/logger');
+
 const { PORT, MONGODB_URL } = require('./utils/config');
 
 const app = express();
@@ -23,10 +29,13 @@ app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
 
+app.use(requestLogger);
+
 app.use(router);
 
-app.use(errors());
+app.use(errorLogger);
 
+app.use(errors());
 app.use(serverError);
 
 app.listen(PORT, () => {
